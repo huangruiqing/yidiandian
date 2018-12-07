3ab22e8cbe519f3263718e2ae4e02223a4c11b1a
LAPTOP-QSK0UKTK\huangrq <huangruiqing@baiwang.com>
2018-12-07 14:38:23 +0800

    init

---
+++ springboot001/src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -6580508178871187442L;
+    private String userName;
+    private int age;
+
+    public User() {}
+    public User(String userName, int age) {
+        this.age = age;
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
