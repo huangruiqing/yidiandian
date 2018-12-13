85e0c21926f61af838196ff0d42d2153e7a4f2a1
LAPTOP-QSK0UKTK\huangrq <huangruiqing@baiwang.com>
2018-12-13 19:45:00 +0800

    boot 学习内容更新 freemaker

--- springboot001/README.md
+++ springboot001/README.md
@@ -1,7 +1,10 @@
 ### Boot
+```
 > 1、搭建Web  
 > 2、RESTful  
 > 3、thymeleaf  
 > idea 遇到html修改 关闭缓存不生效，加入boot-devps-tool  
 >  需要Bulid xxx.html    
 > 4、swagger  
+> 5、freemaker 作为模板
+```
--- springboot001/src/main/java/com/example/demo/controller/FTLController.java
+++ springboot001/src/main/java/com/example/demo/controller/FTLController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 import java.util.Map;
 
+/**
+ *  freemaker 使用
+ */
 @RestController()
 @EnableAutoConfiguration
 public class FTLController {
