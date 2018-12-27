7b2a4828ee68def2685d27ba3a46b06830a91580
LAPTOP-QSK0UKTK\huangrq <huangruiqing@baiwang.com>
2018-12-27 16:14:41 +0800

    cloud

---
+++ Spring_Cloud/服务注册发现/demo_EurekaSerever/src/main/java/com/example/demo_reka/DemoRekaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo_reka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRekaApplication.class, args);
+    }
+
+}
+
