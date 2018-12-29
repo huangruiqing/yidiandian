bfa01720e227597d2521d2578ee7aeadcae5a060
LAPTOP-QSK0UKTK\huangrq <huangruiqing@baiwang.com>
2018-12-29 14:32:51 +0800

    config from git

---
+++ SpringCloud/read_config_server/src/main/java/com/example/read_config_server/ReadConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.read_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadConfigServerApplication.class, args);
+    }
+
+}
+
