f36ff0b983cd0a687ebe8255ecab5754d4beb27d
LAPTOP-QSK0UKTK\huangrq <huangruiqing@baiwang.com>
2019-01-02 09:46:35 +0800

    gir text

--- SpringCloud/consumer_server/src/main/java/com/example/consumer_server/ConsumerServerApplication.java
+++ SpringCloud/consumer_server/src/main/java/com/example/consumer_server/ConsumerServerApplication.java
@@ -6,6 +6,7 @@ import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableHystrix
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerServerApplication {
 
