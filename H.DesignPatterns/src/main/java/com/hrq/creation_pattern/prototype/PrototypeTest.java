73080bdc5ca09320ac2e1c2fc0e961347f2be5fb
huangrq <huangruiqing@baiwang.com>
2019-12-18 19:14:47 +0800

    原型模式

---
+++ H.DesignPatterns/src/main/java/com/hrq/creation_pattern/prototype/PrototypeTest.java
@@ -0,0 +1,10 @@
+package com.hrq.creation_pattern.prototype;
+
+/**
+ * @ClassName PrototypeTest
+ * @Description 原型模式test
+ * @Author huangrq
+ * @Date 2019/12/18 19:11
+ */
+public class PrototypeTest {
+}
