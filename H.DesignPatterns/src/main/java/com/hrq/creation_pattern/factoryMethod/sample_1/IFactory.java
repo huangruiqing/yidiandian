9de08544d2664b00f5f5d65aeb55ffe678d699eb
huangrq <huangruiqing@baiwang.com>
2020-01-06 19:54:33 +0800

    修改注释

--- H.DesignPatterns/src/main/java/com/hrq/creation_pattern/factoryMethod/sample_1/IFactory.java
+++ H.DesignPatterns/src/main/java/com/hrq/creation_pattern/factoryMethod/sample_1/IFactory.java
@@ -2,7 +2,7 @@ package com.hrq.creation_pattern.factoryMethod.sample_1;
 
 /**
  * @ClassName IFactory
- * @Description 抽象工厂 抽象工厂：提供了厂品的生成方法
+ * @Description 工厂方法 ：提供了厂品的生成方法
  * @Author huangrq
  * @Date 2019/12/23 16:53
  */
